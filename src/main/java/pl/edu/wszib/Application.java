c1241d92eacaf464b1ad478124a43c026c8d2114
dmiskowi <dmiskowi@student.wszib.edu.pl>
2022-10-02 15:47:58 +0200

    1.0

--- src/main/java/pl/edu/wszib/Application.java
+++ src/main/java/pl/edu/wszib/Application.java
@@ -1,15 +1,23 @@
 package pl.edu.wszib;
 
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        MyClass myClass = new MyClass("1", "2", "3");
+import pl.edu.wszib.myPackage.MyClass7;
 
-        myClass.run1();
-        myClass.run2();
-        //myClass.run3(); private method
+public class App
+{
+    public static void main( String[] args )
+    {
 
+        System.out.println( "Hello World!" );
 
+        MyClass cll;
+        cll = new MyClass();
+
+        //cll.field3 = "Field3";
+        cll.field2 = "Field1";
+        //cll.field4 = "Field4";
+        cll.run2();
+        //cll.run3();
+        cll.run4();
 
     }
 }
--- src/main/java/pl/edu/wszib/myPackage/MyClass7.java
+++ src/main/java/pl/edu/wszib/myPackage/MyClass7.java
@@ -2,6 +2,14 @@ package pl.edu.wszib.myPackage;
 
 import pl.edu.wszib.MyClass;
 
-public class MyClass7 {
-    private MyClass myClass;
+public class MyClass7 extends MyClass
+{
+    //private pl.edu.wszib.myclass cll;
+    public MyClass cll;
+
+    public void run1(){
+
+        cll.run4();
+        //field3 = "field 3 from extended";
+    }
 }
